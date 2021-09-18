ac890b433b599adafe2e6f47d194902cf62aeaa5
redtail <hongwei0522@gmail.com>
2021-09-18 22:20:31 +0800

    all.JS特效

---
+++ all.js
@@ -0,0 +1,14 @@
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+btnScrollToTop.addEventListener("click",function() {
+    window.scrollTo({
+        top : 0 ,
+        left : 0 ,
+        behavior : "smooth"
+    })
+});
+
+
+window.addEventListener('scroll' , () => {
+    var header = document.querySelector('.header');
+    header.style.opacity = "0.8"
+});
