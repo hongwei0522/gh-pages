3d3a5d499e9d9ca3b40ff98c7b55c33dab3f2408
redtail <hongwei0522@gmail.com>
2021-09-19 17:33:09 +0800

    調整JS效果

--- all.js
+++ all.js
@@ -1,14 +1,21 @@
-const btnScrollToTop = document.querySelector("#btnScrollToTop");
-btnScrollToTop.addEventListener("click",function() {
-    window.scrollTo({
-        top : 0 ,
-        left : 0 ,
-        behavior : "smooth"
-    })
-});
+var btn = document.getElementById('x');
+
+btn.addEventListener("click", function() {
+  var i = 3000;
+  var int = setInterval(function() {
+    window.scrollTo(0, i);
+    i -= 20
+    if (i <= 0) clearInterval(int);
+  }, 20);
+})
+
 
 
 window.addEventListener('scroll' , () => {
-    var header = document.querySelector('.header');
-    header.style.opacity = "0.8"
+  var header = document.querySelector('.header');
+  if( document.documentElement.scrollTop > 0){
+  header.style.opacity = "0.8"
+  }else{
+  header.style.opacity = "1"
+  }
 });
